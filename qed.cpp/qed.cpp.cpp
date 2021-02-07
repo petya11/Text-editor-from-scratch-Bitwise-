824788a01005cb0ca8ae2efce29b8446907da438
петр петров <petrovskoe97@mail.ru>
2021-02-07 13:28:24 +0300

    Добавьте файлы проекта.

---
+++ qed.cpp/qed.cpp.cpp
@@ -0,0 +1,15 @@
+﻿#include <pch.h>
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int){
+	WNDCLASSA window_class = { 0 };
+	window_class.hInstance = instance;
+	window_class.lpfnWndProc = DefWindowProcA;
+	window_class.lpszClassName = "qed";
+	RegisterClassA(&window_class);
+
+	HWND window = CreateWindowA("qed", "QED", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, instance, 0);
+
+
+	return 0;   
+}
+
